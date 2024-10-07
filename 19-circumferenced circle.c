496caadb58fc0c088c2413370ef7a906c3802ac5
navin-2006 <navinroxx4@gmail.com>
2024-10-07 22:04:32 +0530

    Create 19-circumferenced circle.c

---
+++ 19-circumferenced circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include<math.h>
+
+int main() {
+     float a,area,pi=3.14;
+     printf("enter the number: ");
+     scanf("%f",&a);
+     area=pi*pow(a/2,2);
+     printf("%f",area);
+     
+    return 0;
+}
