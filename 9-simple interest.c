26060b7a9b5255de5f9d1321e86771eb64ad8a63
navin-2006 <navinroxx4@gmail.com>
2024-10-07 19:59:55 +0530

    Create 9-simple interest.c

---
+++ 9-simple interest.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int p,t,r,SI;
+  printf("enter p:");
+  scanf("%d",&p);
+  printf("enter t:");
+  scanf("%d",&t);
+  printf("enter r:");
+  scanf("%d",&r);
+  SI=(p*t*r)/100;
+  printf("simple interest is %d", SI);
+  
+  return 0;
+
+}
